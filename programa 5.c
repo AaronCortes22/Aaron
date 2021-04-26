50a5703d509226e549e811ed10cd058caf92029c
AaronCortes22 <82549967+AaronCortes22@users.noreply.github.com>
2021-04-26 18:35:10 -0500

    Add files via upload

---
+++ programa 5.c
@@ -0,0 +1,22 @@
+#include < stdio.h >
+int  main ()
+{
+     /* Aaron Rodriguez Cortes
+    Equipo umizumi
+    21 abr 21
+    Programa 5. Pide 2 valores reales, calcula multiplica, divide y muestra resultado
+    */
+
+   cebador flotante , segundo, multiplica, división;
+    printf ( " \ n \ n Programa 5 " );
+    printf ( " \ n \ n Saca el área de un triángulo " );
+    printf ( " \ n \ n Dame el primer valor de la base " );
+    scanf ( " % f " , & cebador); // Permite capturar un valor real y lo guarda en variable primer
+    printf ( " \ n \ n Dame el segundo valor de la altura " );
+    scanf ( " % f " , & segundo); // Permite capturar un valor real y lo guarda en variable primer
+    multiplica = cebador * segundo;
+    division = cebador * segundo / 2
+    printf ( " \ n El área del triángulo % f " , división);
+
+    return  0 ;
+}
